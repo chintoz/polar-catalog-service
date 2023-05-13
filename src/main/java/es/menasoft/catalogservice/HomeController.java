84d3dc50642297a0f7eb4f69ce186e6aba97026e
chintoz <jacintomena@gmail.com>
2023-05-13 17:53:40 +0200

    Initial changes

---
+++ src/main/java/es/menasoft/catalogservice/HomeController.java
@@ -0,0 +1,13 @@
+package es.menasoft.catalogservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String getGreeting() {
+        return "Welcome to the book catalog";
+    }
+}
