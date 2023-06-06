4a9f9f648473bd079a88ee2dc3911a0555ca134a
chintoz <jacintomena@gmail.com>
2023-06-06 22:47:54 +0200

    Changes to fix some problems with existing tests.

--- src/test/java/es/menasoft/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/es/menasoft/catalogservice/CatalogServiceApplicationTests.java
@@ -4,11 +4,13 @@ import es.menasoft.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
 
     @Autowired
@@ -18,8 +20,7 @@ class CatalogServiceApplicationTests {
     void contextLoads() {
     }
 
-    //@Test
-    // TODO: [CHINTOZ]: Fix the test.
+    @Test
     void whenPostRequestThenBookCreated() {
         var expectedBook = Book.of("1231231231", "Title", "Author", 9.90);
         webTestClient
